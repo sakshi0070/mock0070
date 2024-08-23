8b29ff034cb68cfb79f76471d250314a53a92672
sakshi0070 <wakodesakshi830@gmail.com>
2024-08-23 15:46:27 +0530

    srcJava0070 folder is added by sky

---
+++ srcJava0070/day1BasicProgram/firstProgram.java
@@ -0,0 +1,20 @@
+package day1BasicProgram;
+
+public class firstProgram {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hi..");
+		System.out.println("hello world..!");
+		System.out.println("hiiiii");
+	} 
+}
+
+
+
+
+
+
+
+
+
